6e605d6edf0091d86c7398c51b0fa71443e5a61d
Kurosh Hesamshariati <kuroshhesamshariati@gmail.com>
2023-07-29 12:48:46 +0300

    Create Player prefeb
    also some made Variable of some Magic numbers in PlayerController

--- Dancing On Notes/Assets/Scripts/PlayerController.cs
+++ Dancing On Notes/Assets/Scripts/PlayerController.cs
@@ -8,12 +8,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Vector3 _posA;
     [SerializeField] private Vector3 _posB;
     [SerializeField] private float _speed = .5f;
+    [SerializeField] private float _turnRange = 1f;
     private float _current, _target;
     private bool _canTurn;
 
     private void Update()
     {
-        _canTurn = Mathf.Abs(transform.position.x) > 1 ? true : false;
+        _canTurn = Mathf.Abs(transform.position.x) > _turnRange ? true : false;
 
         if (Input.GetMouseButtonDown(0) && _canTurn) _target = _target == 0 ? 1 : 0;
         _current = Mathf.MoveTowards(_current, _target, _speed * Time.deltaTime);
